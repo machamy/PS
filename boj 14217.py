b9c0bdbab2d81c78845d76f04e2da97b2b148050
Macham <macham2001@gmail.com>
2023-01-17 05:09:31 +0900

    14217 클리어

--- boj 14217.py
+++ boj 14217.py
@@ -1,29 +1,65 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 
-
 def solve():
-    n = int(input())
-    prnts = [i for i in range(n)]
+    # n : 도시의 개수
+    # m : 도로의 개수
+    n, m = map(int, input().split())
 
-    k = int(input())
-    for _ in range(k):
-        a, b = map(int, input().split())
-        if find(prnts,a) != find(prnts,b):
-           union(prnts, a, b)
+    # 초기 도시 상태
+    cities = [tuple(map(int, input().split())) for _ in range(m)]
+    # 앞의로의 계획
+    queue = [tuple(map(int, input().split())) for _ in range(int(input()))]
 
-    m = int(input())
-    for _ in range(m):
-        a, b = map(int, input().split())
+    # 길 초기화
+    roads = [set() for _ in range(n)]
+    for a, b in cities:
+        roads[a - 1].add(b - 1)
+        roads[b - 1].add(a - 1)
 
-        print(int(find(prnts, a) == find(prnts, b)))
+    # 최소 거리 초기화
+    def bfs(visit, distances):
+        q = deque([0])
+        visit[0] = True
 
-    #print(prnts)
+        while q:
+            e = q.popleft()
 
-T = int(input())
-for i in range(T):
-    print(f"Scenario {i + 1}:")
+            for nxt in roads[e]:
+                if visit[nxt]:
+                    continue
+                visit[nxt] = True
+                q.append(nxt)
+                distances[nxt] = distances[e] + 1
+
+    visit = [False for _ in range(n)]
+    distances = [0 for _ in range(n)]
+    bfs(visit, distances)
+
+    # 도로 계힉 시작
+    for cmd, a, b in queue:
+        a -= 1
+        b -= 1
+
+        if cmd == 1:
+            roads[a].add(b)
+            roads[b].add(a)
+        elif cmd == 2:
+            roads[a].remove(b)
+            roads[b].remove(a)
+
+        visit = [False for _ in range(n)]
+        bfs(visit, distances)
+
+        for i in range(n):
+            if not visit[i]:
+                distances[i] = -1
+        print(*distances)
+        pass
+
+
+if __name__ == "__main__":
     solve()
-    print()
